1af42b16058bca383005456abcc58d83004084c0
Slavik <rutkovsky2003@gmail.com>
2026-04-17 15:37:47 -0700

    added config env variable, added /me to dockerignore

--- src/main.py
+++ src/main.py
@@ -45,7 +45,7 @@ class ApplicationServices:
         evaluator_model: str = "o4-mini",
         eval_limit: int = 10,
         fit_limit: int = 5,
-        config_file: str = "resources/app_config.json",
+        config_file: str = os.getenv("APP_CONFIG", "resources/app_config.json"),
         include_feedback: bool = False,
     ):
         self.config = validate_app_config(config_file)
